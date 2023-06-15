462c2f25e13c1030d25f69504c7c91c2f8307757
Baha <baha_prog2kurs@mail.ru>
2023-06-15 13:57:13 +0600

    work in notebook

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Profile from './components/Profile';
+import Border from './Border';
+import Repository from './Repository';
 
 const App = () => {
 	return (
@@ -10,9 +12,11 @@ const App = () => {
 			<Header />
 			<Navbar />
 			<Profile />
+			<Border />
+			<Repository />
+
 		</div>
 	);
 }
 
 export default App;
-
---
+++ src/Repository.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Repository = () => {
+	return (
+		<ul>
+			<li>123</li>
+			<li>2</li>
+			<li>23</li>
+		</ul>
+	);
+}
+
+export default Repository;
